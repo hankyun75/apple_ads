a9ba1093307cf080a81c7ac461ada50a8080e1ca
hankyun75@gmail.com <hankyun75@gmail.com>
2026-02-12 03:42:06 +0000

    update practice code

---
+++ models/marts/finance/fct_orders.sql
@@ -0,0 +1,31 @@
+with orders as (
+    select *
+    from {{ref("stg_jaffle_shop__orders")}}
+),
+
+payments as (
+    select *
+    from {{ref("stg_stripe__payments")}}
+),
+
+orders_payments as (
+    select 
+        payments.order_id,
+        sum(case when status = 'success' then amount end) as amount
+    from payments
+    group by payments.order_id
+),
+
+final as (
+    select 
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce(orders_payments.amount, 0) as amount
+    from orders
+    left join orders_payments
+    using (order_id)
+)
+
+select *
+from final
